60cfba08cc1e9ef96e40f47b3863c0a308e1ca24
Ian Vieira <iansalgado@yahoo.com.br>
2017-08-17 02:19:03 -0300

    testing s.io

--- server/server.js
+++ server/server.js
@@ -1,20 +1,32 @@
 const path = require('path');
+const http = require('http'); // modulo built in
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const socketIO = require('socket.io');
 
 // config express 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server); // integrando S.IO com o server e express
+
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(
     express.static(publicPath));
 
+    io.on('connection', (socket)=>{ // registra um event listener
+        console.log('New User Connected');
 
+        socket.on('disconnect', ()=>{
+            console.log("User left.");
+        });
 
+    });
+   
 
 // listen to
-app.listen(port,
+server.listen(port,
     () =>{
         console.log(`Server is up on port ${port}`);
     }
